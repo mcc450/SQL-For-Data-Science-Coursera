955a9f4530c473b98f4aa677d3d4693674cf6133
Michael McCarthy <103532148+mcc450@users.noreply.github.com>
2022-11-01 15:09:16 -0600

    Update Yelp_Data_Project.sql

--- Yelp_Data_Project.sql
+++ Yelp_Data_Project.sql
@@ -47,9 +47,8 @@
 
 -- 3. Are there any columns with null values in the Users table? Indicate "yes," or "no."
 
-	-- Answer:
-	
-	
+	-- Answer: no
+
 	-- SQL code used to arrive at answer:
 SELECT COUNT(*)
 FROM user
@@ -73,6 +72,13 @@ WHERE id IS NULL OR
       compliment_funny IS NULL OR 
       compliment_writer IS NULL OR 
       compliment_photos IS NULL;
+      
+ -- Result:
++----------+
+| COUNT(*) |
++----------+
+|        0 |
++----------+
 	
 
 	
@@ -114,7 +120,35 @@ GROUP BY city
 ORDER BY total_reviews DESC;
 	
 	-- Copy and Paste the Result Below:
-	
++-----------------+--------------+
+| city            | total_review |
++-----------------+--------------+
+| Las Vegas       |        82854 |
+| Phoenix         |        34503 |
+| Toronto         |        24113 |
+| Scottsdale      |        20614 |
+| Charlotte       |        12523 |
+| Henderson       |        10871 |
+| Tempe           |        10504 |
+| Pittsburgh      |         9798 |
+| Montréal        |         9448 |
+| Chandler        |         8112 |
+| Mesa            |         6875 |
+| Gilbert         |         6380 |
+| Cleveland       |         5593 |
+| Madison         |         5265 |
+| Glendale        |         4406 |
+| Mississauga     |         3814 |
+| Edinburgh       |         2792 |
+| Peoria          |         2624 |
+| North Las Vegas |         2438 |
+| Markham         |         2352 |
+| Champaign       |         2029 |
+| Stuttgart       |         1849 |
+| Surprise        |         1520 |
+| Lakewood        |         1465 |
+| Goodyear        |         1155 |
++-----------------+--------------+
 
 	
 -- 6. Find the distribution of star ratings to the business in the following cities:
