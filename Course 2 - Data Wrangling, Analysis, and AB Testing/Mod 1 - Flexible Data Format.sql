5020d66eed9b96b9dfda56298c1fb4050c7f02e2
Michael McCarthy <103532148+mcc450@users.noreply.github.com>
2022-11-07 09:00:49 -0700

    Create Mod 1 - Flexible Data Format.sql

---
+++ Course 2 - Data Wrangling, Analysis, and AB Testing/Mod 1 - Flexible Data Format.sql
@@ -0,0 +1,108 @@
+-- Exercise 1:
+-- Goal: Write a query to format the view_item event into a table with the appropriate columns.
+
+SELECT 
+  event_id,
+  event_time,
+  user_id,
+  platform,
+  parameter_name,
+  parameter_value
+FROM 
+  dsv1069.events
+WHERE 
+  event_name = 'view_item'
+  
+---------------------------------------------------------------------------------------------
+  
+-- Exercise 2:
+-- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
